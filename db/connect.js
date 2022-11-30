01c24d10857c446636fba2be6443e9a9e34d39f6
Soham Sarkar <pes2202100825@pesu.pes.edu>
2022-12-01 05:08:08 +0530

    added gitignore to secure my mongo atlas connection.

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectionString='mongodb+srv://sohoxic:Piku@2411@cluster0.icr0bt8.mongodb.net/task-manager?retryWrites=true&w=majority'
+const connectionString='mongodb+srv://sohoxic:taskmanager@cluster0.icr0bt8.mongodb.net/task-manager?retryWrites=true&w=majority'
 
 const connectDB = (url) => {
   return mongoose.connect(connectionString, {
